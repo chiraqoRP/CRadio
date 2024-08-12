f85e1cb386b75d31b5bc87ec99ce6b9b1fcc4576
afxnatic <174385871+afxnatic@users.noreply.github.com>
2024-08-11 20:16:24 -0700

    removed useless sh_init.lua

--- lua/cradio/shared/sh_init.lua
+++
@@ -1,7 +0,0 @@
--- Construct our Core/Main class.
--- CRadio = CRadio or CRadioClass()
-
--- if SERVER then
--- 	util.AddNetworkString("CRadio_NetworkPlaylist")
--- 	util.AddNetworkString("CRadio_RequestStatusChange")
--- end
