36d43a6055cdf0100464023ab950cdd604464415
afxnatic <174385871+afxnatic@users.noreply.github.com>
2024-11-06 20:57:50 -0800

    updated example_station.lua to use structs

--- example_station.lua
+++ example_station.lua
@@ -1,24 +1,18 @@
 ---------------------------------
 -- Station
 ---------------------------------
-local station = CRadio:Station("My Station")
-station:SetIcon("cradio/stations/my_station.png")
+local station = CRadio:Station("My Station", {
+    Icon = "cradio/stations/my_station.png"
+})
 
 ---------------------------------
 -- Songs
 ---------------------------------
-local song = CRadio:Song("Cool Song 1")
-song:SetArtist("Cool Artist 1")
-song:SetRelease("Cool Release 1")
-song:SetLength(485.35)
-song:SetURL("https://coolfile.host/cool_song_1.mp3")
-song:SetCover("cradio/covers/cool_release_1.png")
-song:SetParent(station)
-
-song = CRadio:Song("Cool Song 2")
-song:SetArtist("Cool Artist 2")
-song:SetRelease("Cool Release 2")
-song:SetLength(469.24)
-song:SetURL("https://coolfile.host/cool_song_2.mp3")
-song:SetCover("cradio/covers/cool_release_2.png")
-song:SetParent(station)
+CRadio:Song("Cool Song 1", {
+    Artist = "Cool Artist 1",
+    Release = "Cool Release 1",
+    Length = 485.35,
+    URL = "https://coolfile.host/cool_song_1.mp3",
+    Cover = "cradio/covers/cool_release_1.png",
+    Parent = station
+})
