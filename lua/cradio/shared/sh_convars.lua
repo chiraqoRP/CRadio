ea441d94ddadeb1c580977fa0cc0817fda1a95e8
afxnatic <174385871+afxnatic@users.noreply.github.com>
2024-12-15 23:31:19 -0800

    spawnmenu settings

--- lua/cradio/shared/sh_convars.lua
+++ lua/cradio/shared/sh_convars.lua
@@ -1,10 +1,10 @@
 if CLIENT then
-    CreateClientConVar("cl_cradio", 1, true, false, "", 0, 1)
-    CreateClientConVar("cl_cradio_prebuffer", 1, true, false, "", 0, 1)
-    CreateClientConVar("cl_cradio_notification", 1, true, false, "", 0, 1)
-    CreateClientConVar("cl_cradio_volume", 1.0, true, false, "", 0, 2.0)
-    CreateClientConVar("cl_cradio_lower_on_speak", 1, true, false, "", 0, 1)
-    CreateClientConVar("cl_cradio_gui_spawnmenu", 1, true, false, "Enables or disables overriding the spawnmenu.", 0, 1)
+    CreateClientConVar("cl_cradio", 1, true, false, "Enables/disables the radio by stopping channel creation and disallowing opening the GUI.", 0, 1)
+    CreateClientConVar("cl_cradio_prebuffer", 1, true, false, "Makes the upcoming track for your current station preload.", 0, 1)
+    CreateClientConVar("cl_cradio_notification", 1, true, false, 'Shows the "now playing" notification on station change or next track.', 0, 1)
+    CreateClientConVar("cl_cradio_volume", 1.0, true, false, "Sets the volume of all audio channels. Defaults to 1.0.", 0, 2.0)
+    CreateClientConVar("cl_cradio_lower_on_speak", 1, true, false, "Lowers the volume of the current station's audio channel when players you can hear are speaking.", 0, 1)
+    CreateClientConVar("cl_cradio_gui_spawnmenu", 1, true, false, "Enables the GUI opening instead of the spawnmenu when the relevant bind is pressed.", 0, 1)
 
     cvars.AddChangeCallback("cl_cradio", function(name, old, new)
         if old == new then
@@ -136,6 +136,26 @@ if CLIENT then
 
             vehicle.StaticSound = nil
         end
+    end)
+
+    local function RadioSettings(panel)
+        panel:Help("Toggles")
+        panel:CheckBox("Enable?", "cl_cradio")
+        panel:CheckBox("Enable prebuffering?", "cl_cradio_prebuffer")
+        panel:CheckBox('Enable "now playing" notification?', "cl_cradio_notification")
+        panel:CheckBox("Enable volume lowering on player speaking?", "cl_cradio_lower_on_speak")
+        panel:CheckBox("Enable spawnmenu override?", "cl_cradio_gui_spawnmenu")
+    
+        panel:Help("Vars")
+        panel:NumSlider("Volume", "cl_cradio_volume", 0, 2.0, 1)
+    end
+    
+    hook.Add("PopulateToolMenu", "CRadio.Settings", function()
+        spawnmenu.AddToolMenuOption("Options", "CRadio", "CRadio", "Settings", "", "", function(panel)
+            panel:ClearControls()
+    
+            RadioSettings(panel)
+        end)
     end)
 else
     -- server_cvars
